938766f6fca05d4aa035ca91dc104da70854321f
Robin Baby <robin.robinbaby007@gmail.com>
2023-12-12 15:53:40 +0530

    list view related work

--- lib/presentation/downloads/screen/screen_downloads.dart
+++ lib/presentation/downloads/screen/screen_downloads.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:netflix/presentation/common_widgets/widget_app_bar.dart';
 
 class ScreenDownloads extends StatelessWidget {
@@ -6,18 +7,32 @@ class ScreenDownloads extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: PreferredSize(
+    return Scaffold(
+      appBar: const PreferredSize(
         preferredSize: Size.fromHeight(100),
         child: WidgetAppBar(title: 'Downloads'),
       ),
       body: SafeArea(
-        child: Center(
-          child: Text(
-            'Downloads',
-          ),
+          child: Padding(
+        padding: const EdgeInsets.only(left: 10, right: 10),
+        child: ListView(
+          children: [
+            Row(
+              children: [
+                const Icon(Icons.settings, color: Colors.white),
+                const SizedBox(width: 16),
+                Text(
+                  "Smart Download",
+                  style: TextStyle(
+                      fontFamily: GoogleFonts.montserrat().fontFamily,
+                      fontSize: 10,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ],
         ),
-      ),
+      )),
     );
   }
 }
